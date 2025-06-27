1c4531e011c662745b91dbfed6e6df50f50b0077
roger <roger.miret@in2.es>
2025-06-27 12:35:16 +0200

    test helpers

---
+++ src/app/features/credential-details/helpers/actions-helpers.spec.ts
@@ -0,0 +1,59 @@
+// src/app/core/helpers/credential-helpers.spec.ts
+
+import { CredentialStatus } from 'src/app/core/models/entity/lear-credential';
+import { DetailsCredentialType } from 'src/app/core/models/schemas/credential-details-schemas';
+import { credentialTypeHasSendReminderButton, credentialTypeHasSignCredentialButton, statusHasSendReminderlButton, statusHasSignCredentialButton } from './actions-helpers';
+
+describe('Credential Helpers', () => {
+  describe('credentialTypeHasSendReminderButton', () => {
+    const allowed: DetailsCredentialType[] = ['LEARCredentialEmployee', 'gx:LabelCredential'];
+    const disallowed: any = 'SomeOtherType';
+
+    it.each(allowed)('returns true for allowed type %s', (type) => {
+      expect(credentialTypeHasSendReminderButton(type)).toBeTruthy()
+    });
+
+    it('returns false for a disallowed type', () => {
+      expect(credentialTypeHasSendReminderButton(disallowed)).toBeFalsy();
+    });
+  });
+
+  describe('credentialTypeHasSignCredentialButton', () => {
+    const allowed: DetailsCredentialType[] = ['LEARCredentialEmployee', 'VerifiableCertification', 'gx:LabelCredential'];
+    const disallowed: any = 'AnotherType';
+
+    it.each(allowed)('returns true for allowed type %s', (type) => {
+      expect(credentialTypeHasSignCredentialButton(type)).toBeTruthy();
+    });
+
+    it('returns false for a disallowed type', () => {
+      expect(credentialTypeHasSignCredentialButton(disallowed)).toBeFalsy();
+    });
+  });
+
+  describe('statusHasSendReminderlButton', () => {
+    const allowed: CredentialStatus[] = ['WITHDRAWN', 'DRAFT', 'PEND_DOWNLOAD'];
+    const disallowed: any = 'PUBLISHED';
+
+    it.each(allowed)('returns true for allowed status %s', (status) => {
+      expect(statusHasSendReminderlButton(status)).toBeTruthy();
+    });
+
+    it('returns false for a disallowed status', () => {
+      expect(statusHasSendReminderlButton(disallowed)).toBeFalsy();
+    });
+  });
+
+  describe('statusHasSignCredentialButton', () => {
+    const allowed: CredentialStatus[] = ['PEND_SIGNATURE'];
+    const disallowed: any = 'DRAFT';
+
+    it.each(allowed)('returns true for allowed status %s', (status) => {
+      expect(statusHasSignCredentialButton(status)).toBeTruthy();
+    });
+
+    it('returns false for a disallowed status', () => {
+      expect(statusHasSignCredentialButton(disallowed)).toBeFalsy();
+    });
+  });
+});
