977a45d92f25314dc89cde021f5baa70d68d4c6e
Mike <miguel.mir@in2.es>
2025-08-26 17:01:57 +0200

    (issuer) [2.x] update api_path

--- src/app/core/constants/api-paths.constants.ts
+++ src/app/core/constants/api-paths.constants.ts
@@ -3,10 +3,10 @@ export const API_PATH = Object.freeze({
     CREDENTIAL_OFFER: '/oid4vci/v1/credential-offer',
     CLOUD_PROVIDER:'/backoffice/v1/signatures/cloud-providers',
     CONFIGURATION: '/backoffice/v1/configuration',
-    NOTIFICATION: '/api/v1/notifications',
-    PROCEDURES: '/api/v1/procedures',
+    NOTIFICATION: '/backoffice/v1/notifications',
+    PROCEDURES: '/backoffice/v1/procedures',
     REVOKE: '/backoffice/v1/credentials/status/revoke',
     SAVE_CREDENTIAL: '/backoffice/v1/issuances',
     SIGNATURE_CONFIG:'/backoffice/v1/signatures/configs',
-    SIGN_CREDENTIAL: '/api/v1/retry-sign-credential'
+    SIGN_CREDENTIAL: '/backoffice /v1/retry-sign-credential'
 });
